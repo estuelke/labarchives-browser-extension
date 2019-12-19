6648787f69090ce009c6540dd6d6f7f5f2e5040e
Erin Stuelke <erin_stuelke@med.unc.edu>
2019-12-19 18:22:46 -0500

    added toggle functionality to dropdown list, cleaned up styles

--- src/js/cure-center-content.js
+++ src/js/cure-center-content.js
@@ -1,30 +1,88 @@
 function updatePage() {
     if($('.ep_wrapper').length !== 0) {
         addEntryList();
+        observeEntryDisplayResize();
     }
 }
 
+
+function setToggleEvent() {
+    $('#entry-list-toggle').on('click', function () {
+        $(this).children('i').toggleClass('ms-Icon--ChevronLeftSmall');
+        $(this).children('i').toggleClass('ms-Icon--ChevronRightSmall');
+
+        $('#entry-list-column').css('visibility') === 'hidden' ?
+            $('#entry-list-column').css('visibility', 'visible') :
+            $('#entry-list-column').css('visibility', 'hidden');
+
+        $('#entry-list-column').width() === 0 ?
+            $('#entry-list-column').css({ 'width': '20%' }) :
+            $('#entry-list-column').width(0);
+
+        $('#entry-column').css({ 'width': getEntryColumnWidth() })
+    });
+}
+
+
 function addEntryList() {
     if($(`#entry_display_container`).length === 0){
-
+        // Entries from LabArchives
         $('#entry_display').wrapInner(
             `<div id="entry_display_container" class="container-fluid">
-                <div class="row mt-2">
-                    <div class="col-lg-8 col-lg-offset-5"></div>
+                <div class="row position-relative">
+                    <div class="col-lg-auto position-fixed mx-0" id="entry-column"></div>
                 </div>
             </div>`
         );
         
+        // Entry navigation list
         $('#entry_display_container .row').prepend(
-            `<div class="col-lg-4 position-relative">
-                <div class="col-lg-3 position-fixed">
-                    <ul class="list-group" id="custom_entry_list"></ul>
+            `<div class="col-lg-auto position-fixed border-right d-flex pl-1" id="entry-list-column">
+                <ul class="list-group mb-2 flex-fill" id="custom_entry_list"></ul>
+            </div>`
+        );
+        
+        // Entry navigaton list toggle button
+        $('#entry_display_container .row').prepend(
+            `<div class="col-lg-auto position-fixed px-1" id="entry-button-column">
+                <div class="mt-3" style="font-size: 16px">
+                    <button type="button" class="btn btn-sm btn-outline-dark px-1" id="entry-list-toggle">
+                        <i class="ms-Icon ms-Icon--ChevronLeftSmall" aria-hidden="true"></i>
+                    </button>    
                 </div>
             </div>`
         );
-
+        setToggleEvent();
+        setEntryColumnCSS();
         addEntriesToList();
     }
+}
+
+function setEntryColumnCSS() {
+    $('#entry-button-column').css({
+        'height': $('#page-bgtop').outerHeight() - $('#page_header').outerHeight(), 
+        'width': '32px',
+        'overflow-y': 'auto'
+    });
+    
+    $('#entry-list-column').css({
+        'height': $('#page-bgtop').outerHeight() - $('#page_header').outerHeight(), 
+        'width': '20%',
+        'left': $('.la-drawer').outerWidth() + $('#entry-button-column').outerWidth() + parseInt($('#content').css('padding-left'), 10),
+        'overflow-y': 'auto'
+    });
+
+    $('#entry-column').css({
+        'height': $('#page-bgtop').outerHeight() - $('#page_header').outerHeight(),
+        'width': getEntryColumnWidth(),
+        'overflow-y': 'auto',
+        'right': 0,
+        'transition': 'all .5s linear'
+    });
+}
+
+function getEntryColumnWidth() {
+    return $('#entry_display').innerWidth() - $('#entry-list-column').outerWidth() - $('#entry-button-column').outerWidth();
 }
 
 function addEntriesToList() {
@@ -124,12 +182,26 @@ function getListIcon(type) {
     return icon[type] || icon['default'];
 }
 
-setTimeout(()=>updatePage(), 1000);
+function observeEntryDisplayResize() {
+    const entryDisplay = $('#entry_display')[0];
+    const observer = new ResizeObserver(entries => {
+        entries.forEach(entry => {
+            setTimeout(() => $('#entry-column').outerWidth(getEntryColumnWidth()), 300);
+        })
+    })
+
+    observer.observe(entryDisplay);
+}
+
 
 $(document).ready(function() {
-    setTimeout(()=>updatePage(), 1000);
-
+    setTimeout(()=>updatePage(), 2000);
+    
     $(document).on('click', '.la-treenode' ,() => {
-        setTimeout(()=>updatePage(), 1000);
+        setTimeout(()=>updatePage(), 2000);
+    });
+
+    $('#search-submit').on('click', () => {
+        setTimeout(() => updatePage(), 2000);
     });
 });
