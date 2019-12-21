ebc7c8766b2b4681a1bc94175d07c38c1e4f0368
Erin Stuelke <erin_stuelke@med.unc.edu>
2019-12-21 15:09:28 -0500

    added linting and formatting

--- src/js/cure-center-content.js
+++ src/js/cure-center-content.js
@@ -1,5 +1,5 @@
 function updatePage() {
-  if ($(".ep_wrapper").length !== 0) {
+  if ($('.ep_wrapper').length !== 0) {
     addEntryList();
     observeEntryDisplayResize();
     observeChangeInEntryDisplay();
@@ -7,30 +7,30 @@ function updatePage() {
 }
 
 function setToggleEvent() {
-  $("#entry-list-toggle").on("click", function() {
+  $('#entry-list-toggle').on('click', function() {
     $(this)
-      .children("i")
-      .toggleClass("ms-Icon--ChevronLeftSmall");
+      .children('i')
+      .toggleClass('ms-Icon--ChevronLeftSmall');
     $(this)
-      .children("i")
-      .toggleClass("ms-Icon--ChevronRightSmall");
+      .children('i')
+      .toggleClass('ms-Icon--ChevronRightSmall');
 
-    $("#entry-list-column").css("visibility") === "hidden"
-      ? $("#entry-list-column").css("visibility", "visible")
-      : $("#entry-list-column").css("visibility", "hidden");
+    $('#entry-list-column').css('visibility') === 'hidden'
+      ? $('#entry-list-column').css('visibility', 'visible')
+      : $('#entry-list-column').css('visibility', 'hidden');
 
-    $("#entry-list-column").width() === 0
-      ? $("#entry-list-column").css({ width: "20%" })
-      : $("#entry-list-column").width(0);
+    $('#entry-list-column').width() === 0
+      ? $('#entry-list-column').css({ width: '20%' })
+      : $('#entry-list-column').width(0);
 
-    $("#entry-column").css({ width: getEntryColumnWidth() });
+    $('#entry-column').css({ width: getEntryColumnWidth() });
   });
 }
 
 function addEntryList() {
-  if ($(`#entry_display_container`).length === 0) {
+  if ($('#entry_display_container').length === 0) {
     // Entries from LabArchives
-    $("#entry_display").wrapInner(
+    $('#entry_display').wrapInner(
       `<div id="entry_display_container" class="container-fluid">
                 <div class="row position-relative">
                     <div class="col-auto position-fixed mx-0" id="entry-column"></div>
@@ -39,14 +39,14 @@ function addEntryList() {
     );
 
     // Entry navigation list
-    $("#entry_display_container .row").prepend(
+    $('#entry_display_container .row').prepend(
       `<div class="col-auto position-fixed border-right d-flex pl-1" id="entry-list-column">
                 <ul class="list-group mb-2 flex-fill" id="custom_entry_list"></ul>
             </div>`
     );
 
     // Entry navigaton list toggle button
-    $("#entry_display_container .row").prepend(
+    $('#entry_display_container .row').prepend(
       `<div class="col-auto position-fixed px-1" id="entry-button-column">
                 <div class="mt-3" style="font-size: 16px">
                     <button type="button" class="btn btn-sm btn-outline-dark px-1" id="entry-list-toggle">
@@ -62,56 +62,56 @@ function addEntryList() {
 }
 
 function setEntryColumnCSS() {
-  $("#entry-button-column").css({
-    height: "200px", //$('#entry_display').outerHeight(),
-    width: "32px",
-    "overflow-y": "auto"
+  $('#entry-button-column').css({
+    height: '200px', // $('#entry_display').outerHeight(),
+    width: '32px',
+    'overflow-y': 'auto'
   });
 
-  $("#entry-list-column").css({
-    height: $("#entry_display").outerHeight(),
-    width: "20%",
+  $('#entry-list-column').css({
+    height: $('#entry_display').outerHeight(),
+    width: '20%',
     left: getEntryListColumnLeftOffset(),
-    "overflow-y": "auto"
+    'overflow-y': 'auto'
   });
 
-  $("#entry-column").css({
-    height: $("#entry_display").outerHeight(),
+  $('#entry-column').css({
+    height: $('#entry_display').outerHeight(),
     width: getEntryColumnWidth(),
-    "overflow-y": "auto",
+    'overflow-y': 'auto',
     right: 0,
-    transition: "all .5s linear"
+    transition: 'all .5s linear'
   });
 }
 
 function getEntryColumnWidth() {
   return (
-    $("#entry_display").innerWidth() -
-    $("#entry-list-column").outerWidth() -
-    $("#entry-button-column").outerWidth()
+    $('#entry_display').innerWidth() -
+    $('#entry-list-column').outerWidth() -
+    $('#entry-button-column').outerWidth()
   );
 }
 
 function getEntryListColumnLeftOffset() {
   return (
-    $(".la-drawer").outerWidth() +
-    $("#entry-button-column").outerWidth() +
-    parseInt($("#content").css("padding-left"), 10)
+    $('.la-drawer').outerWidth() +
+    $('#entry-button-column').outerWidth() +
+    parseInt($('#content').css('padding-left'), 10)
   );
 }
 
 function addEntriesToList() {
-  const entryWrappers = $(".ep_wrapper");
-  $("#content").data("num-entries", entryWrappers.length);
+  const entryWrappers = $('.ep_wrapper');
+  $('#content').data('num-entries', entryWrappers.length);
 
   entryWrappers.each(function() {
-    const wrapperId = $(this).attr("id");
-    const id = wrapperId.split("_")[1];
+    const wrapperId = $(this).attr('id');
+    const id = wrapperId.split('_')[1];
     const entryId = `ept_${id}`;
     const entry = $(`#${entryId}`);
     const listElement = createListElement(entry, entryId);
 
-    $("#custom_entry_list").append(
+    $('#custom_entry_list').append(
       `<a href="#${wrapperId}" class="list-group-item list-group-item-action">${listElement}</a>`
     );
   });
@@ -122,24 +122,24 @@ function createListElement(entry, entryId) {
   let innerText;
 
   if (
-    entry.hasClass("ep_data") &&
+    entry.hasClass('ep_data') &&
     $(`#${entryId} .la_attach_caption`).length !== 0
   ) {
-    const fileInfo = getFileInfo(entry.find(".la_attach_caption").text());
+    const fileInfo = getFileInfo(entry.find('.la_attach_caption').text());
     icon = getListIcon(fileInfo.extension);
     innerText = `<div>${fileInfo.name}</div><div>${fileInfo.description}</div>`;
   } else if (
-    entry.hasClass("ep_data") &&
+    entry.hasClass('ep_data') &&
     $(`#${entryId} iframe`).length !== 0
   ) {
-    icon = getListIcon("widget");
-    innerText = getInnerText(entry, "Widget");
-  } else if (entry.hasClass("ep_rich_text")) {
-    icon = getListIcon("rich_text");
-    innerText = getInnerText(entry, "Rich Text Entry");
+    icon = getListIcon('widget');
+    innerText = getInnerText(entry, 'Widget');
+  } else if (entry.hasClass('ep_rich_text')) {
+    icon = getListIcon('rich_text');
+    innerText = getInnerText(entry, 'Rich Text Entry');
   } else {
-    icon = getListIcon("default");
-    innerText = getInnerText(entry, "Other");
+    icon = getListIcon('default');
+    innerText = getInnerText(entry, 'Other');
   }
 
   const element = `
@@ -152,10 +152,10 @@ function createListElement(entry, entryId) {
 }
 
 function getInnerText(entry, defaultText) {
-  if (entry.parent().children(".div_tree_path").length !== 0) {
+  if (entry.parent().children('.div_tree_path').length !== 0) {
     const parent = entry.parent();
-    const treePath = parent.children(".div_tree_path");
-    const link = treePath.children("a");
+    const treePath = parent.children('.div_tree_path');
+    const link = treePath.children('a');
     return `${link.text()}<br><span class="text-muted">${defaultText}</span>`;
   } else {
     return defaultText;
@@ -163,14 +163,17 @@ function getInnerText(entry, defaultText) {
 }
 
 function getFileInfo(caption) {
-  caption = caption.replace(/[\u200B]/g, "");
+  caption = caption.replace(/[\u200B]/g, '');
   const [fileInfo, description] = caption.split(/\u00A0/g);
-  filename = fileInfo.substring(0, fileInfo.lastIndexOf("("));
-  fileSize = fileInfo.substring(
-    fileInfo.lastIndexOf("(") + 1,
-    fileInfo.lastIndexOf(")")
+  const filename = fileInfo.substring(0, fileInfo.lastIndexOf('('));
+  const fileSize = fileInfo.substring(
+    fileInfo.lastIndexOf('(') + 1,
+    fileInfo.lastIndexOf(')')
   );
-  extension = filename.substring(filename.lastIndexOf("."), filename.length);
+  const extension = filename.substring(
+    filename.lastIndexOf('.'),
+    filename.length
+  );
 
   return {
     name: filename,
@@ -190,39 +193,39 @@ function getListIcon(type) {
   }
 
   const icon = {
-    ".doc": isOfficeIcon("word"),
-    ".docx": isOfficeIcon("word"),
-    ".xlsx": isOfficeIcon("excel"),
-    ".xls": isOfficeIcon("excel"),
-    ".csv": isOfficeIcon("csv"),
-    ".ppt": isOfficeIcon("powerpoint"),
-    ".pptx": isOfficeIcon("powerpoint"),
-    ".jpg": isGenericIcon("Photo2"),
-    ".jpeg": isGenericIcon("Photo2"),
-    ".png": isGenericIcon("Photo2"),
-    ".svg": isGenericIcon("Photo2"),
-    ".pdf": isGenericIcon("PDF"),
-    ".jmp": isGenericIcon("BarChartVerticalFill"),
-    ".pzfx": isGenericIcon("BarChartVerticalFill"),
-    rich_text: isGenericIcon("Articles"),
-    widget: isGenericIcon("CubeShape"),
-    default: isGenericIcon("KnowledgeArticle")
+    '.doc': isOfficeIcon('word'),
+    '.docx': isOfficeIcon('word'),
+    '.xlsx': isOfficeIcon('excel'),
+    '.xls': isOfficeIcon('excel'),
+    '.csv': isOfficeIcon('csv'),
+    '.ppt': isOfficeIcon('powerpoint'),
+    '.pptx': isOfficeIcon('powerpoint'),
+    '.jpg': isGenericIcon('Photo2'),
+    '.jpeg': isGenericIcon('Photo2'),
+    '.png': isGenericIcon('Photo2'),
+    '.svg': isGenericIcon('Photo2'),
+    '.pdf': isGenericIcon('PDF'),
+    '.jmp': isGenericIcon('BarChartVerticalFill'),
+    '.pzfx': isGenericIcon('BarChartVerticalFill'),
+    rich_text: isGenericIcon('Articles'),
+    widget: isGenericIcon('CubeShape'),
+    default: isGenericIcon('KnowledgeArticle')
   };
 
-  return icon[type] || icon["default"];
+  return icon[type] || icon.default;
 }
 
 function observeEntryDisplayResize() {
-  const entryDisplay = $("#entry_display")[0];
+  const entryDisplay = $('#entry_display')[0];
   const observer = new ResizeObserver(entries => {
     entries.forEach(entry => {
-      const newHeight = $("#entry_display").innerHeight();
+      const newHeight = $('#entry_display').innerHeight();
       setTimeout(
-        () => $("#entry-column").outerWidth(getEntryColumnWidth()),
+        () => $('#entry-column').outerWidth(getEntryColumnWidth()),
         300
       );
-      setTimeout(() => $("#entry-column").outerHeight(newHeight), 300);
-      setTimeout(() => $("#entry-list-column").outerHeight(newHeight), 300);
+      setTimeout(() => $('#entry-column').outerHeight(newHeight), 300);
+      setTimeout(() => $('#entry-list-column').outerHeight(newHeight), 300);
     });
   });
 
@@ -230,23 +233,23 @@ function observeEntryDisplayResize() {
 }
 
 function observeChangeInEntryDisplay() {
-  const content = $("#entry_display")[0];
+  const content = $('#entry_display')[0];
 
   const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
-      if (mutation.type === "childList") {
-        const previousEntries = $("#content").data("num-entries");
-        const currentEntries = $(".ep_wrapper").length;
+      if (mutation.type === 'childList') {
+        const previousEntries = $('#content').data('num-entries');
+        const currentEntries = $('.ep_wrapper').length;
 
         // console.log("before", previousEntries, currentEntries);
         // debugger;
         if (previousEntries !== currentEntries) {
           updatePage();
-          $("#content").data("num-entries", currentEntries);
+          $('#content').data('num-entries', currentEntries);
         }
 
-        console.log("after", previousEntries, currentEntries);
-      } else if (mutation.type === "attributes") {
+        console.log('after', previousEntries, currentEntries);
+      } else if (mutation.type === 'attributes') {
         // console.log(mutation);
       }
     });
@@ -260,13 +263,13 @@ function observeChangeInEntryDisplay() {
 }
 
 function observeEntryDisplayNodeAddition() {
-  const content = $("#content")[0];
+  const content = $('#content')[0];
 
   const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
       const addedNodes = mutation.addedNodes;
       addedNodes.forEach(node => {
-        if ($(node).attr("id") === "entry_display") {
+        if ($(node).attr('id') === 'entry_display') {
           updatePage();
         }
       });
@@ -285,5 +288,5 @@ function defer(fn, condition) {
 }
 
 $(document).ready(function() {
-  defer(observeEntryDisplayNodeAddition, () => $("#content").length !== 0);
+  defer(observeEntryDisplayNodeAddition, () => $('#content').length !== 0);
 });
